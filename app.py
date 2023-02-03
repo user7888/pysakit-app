9ffedea468a53f90a5487dc60d445e5afb868e5d
Elias Junno <junnoelias@gmail.com>
2023-02-03 11:50:39 +0200

    add individual stop route

--- app.py
+++ app.py
@@ -1,52 +1,39 @@
 from flask import Flask
-from flask import render_template, request
-import urllib.request
-import json
+from flask import redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import text
+from os import getenv
+import time
+import datetime
 import requests
+import json
 
 app = Flask(__name__)
+app.run(debug=True)
+app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-@app.route("/")
-def index():
-    words = ["apina", "banaani", "cembalo"]
-    return render_template("index.html", message="Tervetuloa!", items=words)
+@app.route("/template")
+def template():
+    # 'should be declared as text' error fix
+    sql = text("SELECT content FROM messages")
+    result = db.session.execute(sql)
 
-@app.route("/form")
-def form():
-    return render_template("form.html")
+    messages = result.fetchall()
+    return render_template("index_db.html", count=len(messages), messages=messages)
 
-@app.route("/result", methods=["POST"])
-def result():
-    return render_template("result.html", name=request.form["name"])
-
-@app.route("/order")
-def order():
-    return render_template("order.html")
-
-@app.route("/orderresult", methods=["POST"])
-def order_result():
-    pizza = request.form["pizza"]
-    # Huom. getlist-metodi.
-    extras = request.form.getlist("extra")
-    message = request.form["message"]
-    return render_template("order_result.html", pizza=pizza, extras=extras, message=message)
-
-@app.route("/page1")
-def page1():
-    return "Tämä on sivu 1"
-
-@app.route("/page/<int:id>")
-def page(id):
-    return "Tämä on sivu " + str(id)
-
-@app.route("/hsltest")
-def hsl():
-    # Unix timestamp for current time. Fresh timestamp
-    # needed for queries to work.
-    start_time = '1674755608'
+@app.route("/stops/schedules/<int:id>")
+def hsl(id):
+    # Fresh timestamp
+    # needed for queries.
+    current_time = str(time.time()).split(".")
+    start_of_day = datetime.datetime.combine(datetime.datetime.now(), datetime.time.min)
+    print(current_time)
+    start_time = current_time[0]
 
     query = """{
-        stop(id: "HSL:1240133") {
+        stop(id: "HSL:"""+f'{str(id)}"'+""") {
             name
             stoptimesWithoutPatterns(startTime:"""+f'{start_time}'+""") {
                 scheduledArrival
@@ -71,26 +58,29 @@ def hsl():
 
     url = 'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql'
     response = requests.post(url, json={'query': query})
-    return response.text
+    # Convert to dict
+    dict = json.loads(response.text)
+    # Arrival time from response
+    arrival_time = dict['data']['stop']['stoptimesWithoutPatterns'][0]['realtimeArrival']
+    arrival_time_list = dict['data']['stop']['stoptimesWithoutPatterns']
+    sign_and_arrival = []
 
-@app.route("/test")
-def test():
-    query = """query {
-        characters {
-            results {
-                name
-                status
-                species
-                type
-                gender
-            }
-        }
-    }"""
+    for element in arrival_time_list:
+        element_arrival = element['realtimeArrival']
+        time_delta = datetime.timedelta(seconds=element_arrival)
+        date_object = start_of_day + time_delta
+        # Minutes format fix
+        minutes = date_object.minute
+        if minutes < 10:
+            minutes = "0"+str(minutes)
+        temp = (element['headsign'], f'{date_object.hour}:{minutes}')
+        sign_and_arrival.append(temp)
 
-    url = 'https://rickandmortyapi.com/graphql/'
-    response = requests.post(url, json={'query': query})
-
-    print(response.status_code)
-    print(response.text)
-
-    return response.text
+    for element in sign_and_arrival:
+        print(element)
+    # Converting seconds from midnight
+    # to current time.
+    time_delta = datetime.timedelta(seconds=arrival_time)
+    date_object = datetime.datetime.now() + time_delta
+    print(dict['data']['stop']['stoptimesWithoutPatterns'][0]['realtimeArrival'])
+    return render_template('individual_stop.html', arrivals=sign_and_arrival)
