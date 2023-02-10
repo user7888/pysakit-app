2bab33114d4be6c1e8c248e7a7b71282cebcb132
Elias Junno <junnoelias@gmail.com>
2023-02-10 11:01:58 +0200

    fix error in database schema

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
-CREATE TABLE stops_new (id SERIAL PRIMARY KEY, hsl_id TEXT, owner TEXT, visible BOOLEAN);
+DROP TABLE IF EXISTS stops_new, users;
+CREATE TABLE stops_new (id SERIAL PRIMARY KEY, hsl_id TEXT, name TEXT, owner TEXT, visible BOOLEAN);
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT);
