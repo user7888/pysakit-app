bbc15a10a31ffb2f2c8212d0c9a1de6d7293135b
Elias Junno <junnoelias@gmail.com>
2023-02-15 18:44:43 +0200

    update database schema

--- schema.sql
+++ schema.sql
@@ -1,3 +1,3 @@
-DROP TABLE IF EXISTS stops_new, users;
-CREATE TABLE stops_new (id SERIAL PRIMARY KEY, hsl_id TEXT, name TEXT, owner TEXT, visible BOOLEAN);
+DROP TABLE IF EXISTS stops_new, stops, users;
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
+CREATE TABLE stops (id SERIAL PRIMARY KEY, hsl_id TEXT, name TEXT, owner INTEGER REFERENCES users, visible BOOLEAN);
