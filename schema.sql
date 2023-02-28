842237888dee270af7faa9999e46483be015435a
Elias Junno <junnoelias@gmail.com>
2023-02-28 23:38:13 +0200

    update database schema

--- schema.sql
+++ schema.sql
@@ -14,3 +14,26 @@ CREATE TABLE stops (
     owner INTEGER REFERENCES users, 
     visible BOOLEAN
 );
+
+CREATE TABLE routes (
+    id SERIAL PRIMARY KEY, 
+    hsl_id TEXT,
+    short_name TEXT,
+    long_name TEXT, 
+);
+
+CREATE TABLE stops_and_users (
+    id SERIAL PRIMARY KEY, 
+    stop INTEGER REFERENCES stops 
+    user INTEGER REFERENCES users, 
+    visible BOOLEAN
+);
+
+CREATE TABLE routes_and_stops (
+    id SERIAL PRIMARY KEY, 
+    stop INTEGER REFERENCES stops 
+    route INTEGER REFERENCES routes,
+);
+
+
+
